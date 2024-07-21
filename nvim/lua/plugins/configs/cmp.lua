f5092d852f48df1d5fd4ba246b718e966562072d
finnw <finnweidner76@gmail.com>
2024-07-22 00:32:44 +0200

    change cmp style

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -2,71 +2,71 @@ local cmp = require "cmp"
 
 dofile(vim.g.base46_cache .. "cmp")
 
-local cmp_ui = require("core.utils").load_config().ui.cmp
-local cmp_style = cmp_ui.style
-
-local field_arrangement = {
-  atom = { "kind", "abbr", "menu" },
-  atom_colored = { "kind", "abbr", "menu" },
-}
-
-local formatting_style = {
-  -- default fields order i.e completion word + item.kind + item.kind icons
-  fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
-
-  format = function(_, item)
-    local icons = require "nvchad.icons.lspkind"
-    local icon = (cmp_ui.icons and icons[item.kind]) or ""
-
-    if cmp_style == "atom" or cmp_style == "atom_colored" then
-      icon = " " .. icon .. " "
-      item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
-      item.kind = icon
-    else
-      icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
-      item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
-    end
-
-    return item
-  end,
-}
-
-local function border(hl_name)
-  return {
-    { "╭", hl_name },
-    { "─", hl_name },
-    { "╮", hl_name },
-    { "│", hl_name },
-    { "╯", hl_name },
-    { "─", hl_name },
-    { "╰", hl_name },
-    { "│", hl_name },
-  }
-end
+-- local cmp_ui = require("core.utils").load_config().ui.cmp
+-- local cmp_style = cmp_ui.style
+--
+-- local field_arrangement = {
+--   atom = { "kind", "abbr", "menu" },
+--   atom_colored = { "kind", "abbr", "menu" },
+-- }
+--
+-- local formatting_style = {
+--   -- default fields order i.e completion word + item.kind + item.kind icons
+--   fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+--
+--   format = function(_, item)
+--     local icons = require "nvchad.icons.lspkind"
+--     local icon = (cmp_ui.icons and icons[item.kind]) or ""
+--
+--     if cmp_style == "atom" or cmp_style == "atom_colored" then
+--       icon = " " .. icon .. " "
+--       item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+--       item.kind = icon
+--     else
+--       icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+--       item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
+--     end
+--
+--     return item
+--   end,
+-- }
+--
+-- local function border(hl_name)
+--   return {
+--     { "╭", hl_name },
+--     { "─", hl_name },
+--     { "╮", hl_name },
+--     { "│", hl_name },
+--     { "╯", hl_name },
+--     { "─", hl_name },
+--     { "╰", hl_name },
+--     { "│", hl_name },
+--   }
+-- end
 
 local options = {
   completion = {
     completeopt = "menu,menuone",
   },
 
-  window = {
-    completion = {
-      side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-      winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
-      scrollbar = false,
-    },
-    documentation = {
-      border = border "CmpDocBorder",
-      winhighlight = "Normal:CmpDoc",
-    },
-  },
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
+  -- window = {
+  --   completion = {
+  --     side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+  --     winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+  --     scrollbar = false,
+  --   },
+  --   documentation = {
+  --     border = border "CmpDocBorder",
+  --     winhighlight = "Normal:CmpDoc",
+  --   },
+  -- },
+  -- snippet = {
+  --   expand = function(args)
+  --     require("luasnip").lsp_expand(args.body)
+  --   end,
+  -- },
 
-  formatting = formatting_style,
+  -- formatting = formatting_style,
 
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -111,12 +111,11 @@ local options = {
     { name = "buffer" },
     { name = "nvim_lua" },
     { name = "path" },
-    { name = "path" },
   },
 }
 
-if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
-  options.window.completion.border = border "CmpBorder"
-end
+-- if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
+--   options.window.completion.border = border "CmpBorder"
+-- end
 
 return options
